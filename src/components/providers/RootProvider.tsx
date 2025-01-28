9130a6bb16acfb0bd90b3bbfb41817ddeb37a62f
henryhoangduong <henryhoangduong@gmail.com>
2025-01-28 17:38:20 +0700

    auth

---
+++ src/components/providers/RootProvider.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React, { ReactNode } from 'react'
+import { ThemeProvider } from 'next-themes'
+const RootProvider = ({ children }: { children:ReactNode}) => {
+    return (
+      <ThemeProvider attribute="class" defaultTheme='dark' enableSystem disableTransitionOnChange>
+           {children}
+      </ThemeProvider>
+  )
+}
+
+export default RootProvider
