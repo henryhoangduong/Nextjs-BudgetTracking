cda8c99dd731d243a4c55324c03d6210446c06c6
henryhoangduong <henryhoangduong@gmail.com>
2025-02-08 22:40:49 +0700

    category picker

--- src/app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ src/app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -15,20 +15,31 @@ interface Props {
   trigger: ReactNode;
   type: TransactionType;
 }
-import {useForm} from "react-hook-form"
-import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schema/transactions";
-import {zodResolver} from "@hookform/resolvers/zod"
-import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import {
+  CreateTransactionSchema,
+  CreateTransactionSchemaType,
+} from "@/schema/transactions";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CategoryPicker from "./CategoryPicker";
 
 function CreateTransactionDialog({ trigger, type }: Props) {
-    const form = useForm<CreateTransactionSchemaType>({
-        resolver: zodResolver(CreateTransactionSchema),
-        defaultValues: {
-            type,
-            date: new Date(),
-
-        }
-    })
+  const form = useForm<CreateTransactionSchemaType>({
+    resolver: zodResolver(CreateTransactionSchema),
+    defaultValues: {
+      type,
+      date: new Date(),
+    },
+  });
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
@@ -46,10 +57,59 @@ function CreateTransactionDialog({ trigger, type }: Props) {
             </span>
             transaction
           </AlertDialogTitle>
-              </AlertDialogHeader>
-              <Form {...form}>
-                  <form className="space-y-4"></form>
-              </Form>
+        </AlertDialogHeader>
+        <Form {...form}>
+          <form className="space-y-4">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={""} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transaction description (optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={0} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transaction description (required)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between gap-2">
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Category</FormLabel>
+                    <FormControl>
+                      <Input defaultValue={0} {...field} />
+                    </FormControl>
+                    <FormControl>
+                      <CategoryPicker type={type} />
+                    </FormControl>
+                    <FormDescription>Select a category</FormDescription>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
       </AlertDialogContent>
     </AlertDialog>
   );
